6df487577b7e91efee1e10e691e2ead7c1797008
vvithurshan <vvithurshan@gmail.com>
2025-07-18 18:31:21 -0600

    Base Model Done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.state_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -8,6 +9,20 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STATE_NAME = "Prepare base model"
+
+
+try:
+    logger.info(f">>>>>> Stage {STATE_NAME} Started <<<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STATE_NAME} Completed <<<<<<<")
 
 except Exception as e:
     logger.exception(e)
--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -5,6 +5,7 @@ from src.cnnClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
+# from box import ConfigBox
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
