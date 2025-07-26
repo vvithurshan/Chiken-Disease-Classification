5efc15a46da49d37e0449ada5793ae260768273a
vvithurshan <vvithurshan@gmail.com>
2025-07-25 21:15:05 -0600

    Docker Added

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080) # local host
+    app.run(host='0.0.0.0', port=80) # for Azure
